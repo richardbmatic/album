4e3cc5563f1d16d4560a87b972c63e72a5861845
richardbmatic <rmatic@sterlingtech.ph>
2018-10-19 11:24:33 +0800

    My First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link rel="stylesheet" type="text/css" href="/css/app.css">
+        <link rel="stylesheet" type="text/js" href="/js/app.js">
+    </head>
+    <body>
+        <div class="container">
+            <h1 class="display-1">Boom!!!!!!</h1>
+        </div>
+    </body>
+</html>
