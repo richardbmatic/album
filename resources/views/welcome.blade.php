7933b9f98f10cb40d8080ea39e4f043fc27aa73e
richardbmatic <rmatic@sterlingtech.ph>
2018-10-25 09:49:42 +0800

    home banner, feature and about

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,88 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Album</title>
 
         <link rel="stylesheet" type="text/css" href="/css/app.css">
         <link rel="stylesheet" type="text/js" href="/js/app.js">
     </head>
     <body>
-        <div class="container">
-            <h1 class="display-1">Boom!!!!!!</h1>
+        <div class="banner">
+            <nav class="navbar banner-nav absolute-bottom">
+                <div class="container">
+                    <ul class="nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Active</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div class="featured">
+            <div class="featured-item">
+                <div class="featured-image-container col-md-7">
+                    <div class="featured-big"></div>
+                </div>
+                <div class="featured-image-container  col-md-5">
+                    <div class="featured-small"></div>
+                </div>
+            </div>
+            <div class="featured-item">
+                <div class="featured-image-container  col-md-5">
+                    <div class="featured-small"></div>
+                </div>
+                <div class="featured-image-container col-md-7">
+                    <div class="featured-big"></div>
+                </div>
+            </div>
+            <div class="featured-item">
+                <div class="featured-image-container col-md-7">
+                    <div class="featured-big"></div>
+                </div>
+                <div class="featured-image-container  col-md-5">
+                    <div class="featured-small"></div>
+                </div>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="about-us container">
+            <div class="about-us-head"><h3>ABOUT US</h3></div>
+            <div class="col-md-6">
+                <div class="about-us-image">
+                    <img src="/images/about/about_us.jpg" alt="About Us">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="about-us-image">
+                    <h4>lorem Ipsum</h4>
+                    <span>Auris eget vestibulum augue. Integer sit amet tellus libero.
+                        Nam ac enim ut velit dictum congue. 
+                        Integer vitae elit in risus laoreet porttitor non nec enim. 
+                        Donec nec sapien odio. Nullam dictum, elit maximus vulputate venenatis, 
+                        neque velit tincidunt mauris, ac facilisis ligula mi et neque. 
+                        Vivamus ut felis sem. In eget diam lorem. Aliquam tincidunt. 
+                        Aliquam venenatis non orci egestas cursus. 
+                        Phasellus venenatis blandit odio, in tincidunt ex trist. 
+                        Integer sit amet tellus libero.Nam ac enim ut velit dictum congue. 
+                        Integer vitae elit in risus laoreet porttitor non nec enim.
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="grid-continer gallery">
+            <div class="grid-item"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item">`</div>
         </div>
     </body>
 </html>
